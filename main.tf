a60d7ec37b7ff05dc67a17034859d3beaa0697c5
xcevor-notes <127529898+xcevor-notes@users.noreply.github.com>
2023-08-09 21:03:43 +0900

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,66 @@
+terraform {
+ required_providers {
+ kubernetes = {
+ source = "hashicorp/kubernetes"
+ version = "2.11.0"
+ }
+ }
+}
+
+provider "kubernetes" {
+ config_path = "/root/.kube/config"
+}
+
+
+main.tf
+
+resource "kubernetes_deployment" "frontend" {
+ metadata {
+ name = "frontend"
+ labels = {
+ name = "frontend"
+ }
+ }
+ spec {
+ replicas = 4
+ selector {
+ match_labels = {
+ name = "webapp"
+ }
+ }
+ template {
+ metadata {
+ labels = {
+ name = "webapp"
+ }
+ }
+ spec {
+ container {
+ image = "kodekloud/webapp-color:v1"
+ name = "simple-webapp"
+ port {
+ container_port = 8080
+ }
+ }
+ }
+ }
+ }
+}
+
+resource "kubernetes_service" "webapp-service" {
+ metadata {
+ name = "webapp-service"
+ }
+ spec {
+ selector = {
+ name = "webapp"
+ }
+ port {
+ port = 8080
+ target_port = 8080
+ node_port = 30080
+ }
+
+ type = "NodePort"
+ }
+}
