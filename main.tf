3a12cbec139d170ba3a41b9529393a4e0f4dbc4f
xcevor-notes <127529898+xcevor-notes@users.noreply.github.com>
2023-08-09 21:37:49 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,3 @@
-terraform {
- required_providers {
- kubernetes = {
- source = "hashicorp/kubernetes"
- version = "2.11.0"
- }
- }
-}
-
-provider "kubernetes" {
- config_path = "/root/.kube/config"
-}
-
-
-main.tf
-
 resource "kubernetes_deployment" "frontend" {
  metadata {
  name = "frontend"
